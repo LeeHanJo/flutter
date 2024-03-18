d21fca251e2a2312f985a4912e984759506d62a3
LeeHanJo <gps95wwkhhev@gmail.com>
2024-03-18 18:06:47 +0900

    -2024-03-18 last-

--- test1/lib/main.dart
+++ test1/lib/main.dart
@@ -1,125 +1,184 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test1/to_do_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ToDoService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+/// ToDo 클래스
+class ToDo {
+  String job; // 할 일
+  bool isDone; // 완료 여부
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  ToDo(this.job, this.isDone); // 생성자
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+//// 홈 페이지
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Consumer<ToDoService>(
+      builder: (context, toDoService, child) {
+        // toDoService로 부터 toDoList 가져오기
+        List<ToDo> toDoList = toDoService.toDoList;
+
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.greenAccent,
+            title: Text("ToDo 리스트"),
+          ),
+          body: toDoList.isEmpty
+              ? Center(
+                  child: Text("To Do List를 작성해주세요"),
+                )
+              : ListView.builder(
+                  itemCount: toDoList.length,
+                  itemBuilder: (context, index) {
+                    ToDo toDo = toDoList[index];
+                    return ListTile(
+                      title: Text(
+                        toDo.job,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: toDo.isDone ? Colors.grey : Colors.black,
+                          decoration: toDo.isDone
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          CupertinoIcons.delete,
+                        ),
+                        onPressed: () {
+                          //삭제 버튼이 눌렀을 때 작동
+                          toDoService.deleteToDo(index);
+                        },
+                      ),
+                      onTap: () {
+                        //아이템 클릭시
+                        toDo.isDone = !toDo.isDone;
+                        toDoService.updateToDo(toDo, index);
+                      },
+                    );
+                  }),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              // + 버튼 클릭시 ToDo 생성 페이지로 이동
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => CreatePage()),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+/// ToDo 생성 페이지
+class CreatePage extends StatefulWidget {
+  const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  // TextField의 값을 가져올 때 사용
+  TextEditingController textController = TextEditingController();
+
+  // 경고 메세지
+  String? error;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.greenAccent,
+        title: Text("ToDo리스트 작성"),
+        // 뒤로가기 버튼
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.chevron_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            // 텍스트 입력창
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: "To Do List를 작성해주세요",
+                errorText: error,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20),
+            // 추가하기 버튼
+            SizedBox(
+              width: double.infinity,
+              height: 45,
+              child: ElevatedButton(
+                child: Text(
+                  "추가하기",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  // 추가하기 버튼 클릭시
+                  String job = textController.text;
+                  if (job.isEmpty) {
+                    setState(() {
+                      // 내용이 없는 경우 에러 메세지
+                      error = "내용을 입력해주세요.";
+                    });
+                  } else {
+                    setState(() {
+                      // 내용이 있는 경우 에러 메세지 숨김
+                      error = null;
+                    });
+                    ToDoService toDoService = context.read<ToDoService>();
+                    // ToDoService에 있는 createToDo 메스더 사용
+                    toDoService.createToDo(job);
+                    Navigator.pop(context);
+                  }
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
